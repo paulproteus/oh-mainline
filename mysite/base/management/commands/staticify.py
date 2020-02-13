4aa4ff72e9a5f58fc2d025505c8d65fd2548abc9
Asheesh Laroia <asheesh@asheesh.org>
2020-02-13 12:44:31 -0800

    Handle all URLs without crashing
    Also, add dry-run for faster feedback loop.

--- mysite/base/management/commands/staticify.py
+++ mysite/base/management/commands/staticify.py
@@ -6,7 +6,7 @@ import sys
 import urllib
 
 from django.core.management.base import BaseCommand
-from django.core.urlresolvers import RegexURLPattern
+from django.core.urlresolvers import RegexURLPattern, RegexURLResolver
 from django.test.client import Client
 import exrex
 
@@ -33,6 +33,28 @@ SKIPPABLE = [
     '^missions/tar/downloadfile/(?P<name>.*)',
     '^missions/tar/ghello-0.4.tar.gz',
     '^customs/add/(?P<tracker_type>\\w*)$',
+    '^__debug__/',
+    # These URLs depend on templates which have been deleted.
+    '^\\+landing/import$',
+    '^\\+landing/opps$',
+    '^\\+landing/projects$',
+    # This URL is intended to crash.
+    '^account/catch-me$',
+    # This crashes when not logged in.
+    '^profile/views/replace_icon_with_default$',
+    # exrex can't generate a list of valid URLs for these tastypie resources.
+    # Skip them, since they require login.
+    '^(?P<resource_name>portfolio_entry)/set/(?P<pk_list>.*?)/$',
+    '^(?P<resource_name>portfolio_entry)/(?P<pk>.*?)/$',
+    '^(?P<resource_name>tracker_model)/set/(?P<pk_list>.*?)/$',
+    '^(?P<resource_name>tracker_model)/(?P<pk>.*?)/$',
+    # I think these come from django-admin and/or django.contrib.auth. I'm not sure; we don't need them, anyway.
+    '^(.+)/$',
+    '^(\\d+)/password/$',
+    '^(.+)/history/$',
+    '^(.+)/delete/$',
+    '^disconnect/(?P<backend>[^/]+)/(?P<association_id>[^/]+)/$',
+    '^complete/(?P<backend>[^/]+)/$',
 ]
 
 def save_html(url, html):
@@ -52,11 +74,18 @@ def fix_wiki_url(url):
 def save_regex_pattern(c, regex_pattern):
     num_possible = exrex.count(regex_pattern)
     if num_possible <= 2:
-        longest_example = sorted(
-            exrex.generate(regex_pattern),
+        try:
+            all_matches = list(exrex.generate(regex_pattern))
+        except Exception:
+            print 'exrex fail on', repr(regex_pattern)
+            return
+        longest_example = sorted(all_matches,
             key=lambda s: len(s))[-1]
         url = '/' + longest_example
-        response = c.get(url)
+        try:
+            response = c.get(url)
+        except Exception:
+            print('Failed to get url=%s while looking at pattern=%s', url, regex_pattern)
         if response.get('location'):
             html = '<html><head><meta http-equiv="refresh" content="0; URL=\'%s\'" /></head></html>' % (urllib.quote(
                 fix_wiki_url(response['location'].replace('http://testserver', '')), safe='/:'))
@@ -69,21 +98,38 @@ class Command(BaseCommand):
     help = 'Generates a static HTML snapshot of the site'
 
     option_list = BaseCommand.option_list + (
+        make_option('-d', '--dry-run', action='store_true',
+                    dest='dry_run', default=False, help='Skip generating HTML for faster execution.'
+        ),
+        make_option('-m', '--max-work', type='int',
+                    dest='max_work', default=float('inf'), help='Max # of URLs to process'
+        ),
     )
+
+    def add_arguments(self, parser):
+        parser.add_argument('poll_id', nargs='+', type=int)
 
     def handle(self, *args, **options):
         import mysite.urls
-        patterns = mysite.urls.urlpatterns
-        num = 160 # len(patterns)
+        patterns = list(mysite.urls.urlpatterns)
+        i = 0
         c = Client()
-        for pattern in patterns:
+        while i < len(patterns):
+            if i >= options.get('max_work'):
+                return
+            pattern = patterns[i]
+            i += 1
             if isinstance(pattern, RegexURLPattern):
                 regex_pattern = pattern.regex.pattern
                 if regex_pattern in SKIPPABLE:
                     continue
-                save_regex_pattern(c, regex_pattern)
-                num = num - 1
-                if num == 0:
-                    return
+                if not options.get('dry_run'):
+                    save_regex_pattern(c, regex_pattern)
+            elif isinstance(pattern, RegexURLResolver):
+                try:
+                    patterns.extend(pattern.url_patterns)
+                except Exception:
+                    print('whoa')
+                    raise
             else:
-                print('skipping', pattern)
+                print('skipping', repr(pattern))
